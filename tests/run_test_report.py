d3451c64c315d6f5d62c739fb4730f0a09d8f678
unknown <nnaabbcc.zhao@gmail.com>
2019-01-28 20:06:25 +0800

    save the result to specified folder

--- tests/run_test_report.py
+++ tests/run_test_report.py
@@ -1,21 +1,22 @@
 import xmlrunner
 import unittest
 
-import os
-
-import sys
-sys.path.insert(0, '.')
-
 if __name__ == "__main__":
+    import os
+    testsFolder = os.path.dirname(os.path.realpath(__file__))
+    packageFolder = os.path.abspath(os.path.join(testsFolder, os.pardir))
+
+    import sys
+    sys.path.insert(0, packageFolder)
+
     ## Create test suite
     suite = unittest.TestSuite()
 
     ## Add All test cases
-    folder = os.path.dirname(os.path.realpath(__file__))
-    all_test_cases = unittest.defaultTestLoader.discover(folder, 'test_*.py')
+    all_test_cases = unittest.defaultTestLoader.discover(testsFolder, 'test_*.py')
     for test_case in all_test_cases:
         suite.addTest(test_case)
 
     ## Run tests
-    runner = xmlrunner.XMLTestRunner(output='report')
+    runner = xmlrunner.XMLTestRunner(output=os.path.join(packageFolder, 'report'))
     runner.run(suite)
